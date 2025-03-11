439abf0fcd3ebc38dcbb354976b42734b5db85ca
Volodymyr Vasylkivskyi <volodymyr.vasylkivskyi@wickes.co.uk>
2025-03-11 10:09:52 +0200

    implement first weather tool

--- agent.js
+++ agent.js
@@ -1,5 +1,24 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatAnthropic } from "@langchain/anthropic";
+import { z } from "zod";
+
+import { tool } from "@langchain/core/tools";
+
+const weatherTool = tool(
+  async ({ query }) => {
+    console.log("query: ", query);
+    //TODO: implement fetching to API
+
+    return "The weather in Kyiv is warm";
+  },
+  {
+    name: "weather",
+    description: "Get the weather in current location",
+    schema: z.object({
+      query: z.string().describe("The query to use in search"),
+    }),
+  }
+);
 
 const model = new ChatAnthropic({
   model: "claude-3-5-sonnet-latest",
@@ -7,14 +26,14 @@ const model = new ChatAnthropic({
 
 const agent = createReactAgent({
   llm: model,
-  tools: [],
+  tools: [weatherTool],
 });
 
 const result = await agent.invoke({
   messages: [
     {
       role: "user",
-      content: "Hello, how are you?",
+      content: "Hello, what is the weather in Kyiv?",
     },
   ],
 });
