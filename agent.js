9021172ac463962c0c82d55a47cf5aec48294e7b
Volodymyr Vasylkivskyi <volodymyr.vasylkivskyi@wickes.co.uk>
2025-03-11 10:17:01 +0200

    implement MemorySaver

--- agent.js
+++ agent.js
@@ -1,6 +1,7 @@
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatAnthropic } from "@langchain/anthropic";
 import { z } from "zod";
+import { MemorySaver } from "@langchain/langgraph";
 
 import { tool } from "@langchain/core/tools";
 
@@ -24,18 +25,41 @@ const model = new ChatAnthropic({
   model: "claude-3-5-sonnet-latest",
 });
 
+const checkpointSaver = new MemorySaver();
+
 const agent = createReactAgent({
   llm: model,
   tools: [weatherTool],
+  checkpointSaver,
 });
 
-const result = await agent.invoke({
-  messages: [
-    {
-      role: "user",
-      content: "Hello, what is the weather in Kyiv?",
-    },
-  ],
-});
+const result = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "Hello, what is the weather in Kyiv?",
+      },
+    ],
+  },
+  {
+    configurable: { thread_id: 42 },
+  }
+);
+
+const followup = await agent.invoke(
+  {
+    messages: [
+      {
+        role: "user",
+        content: "What city I asked in previous question?",
+      },
+    ],
+  },
+  {
+    configurable: { thread_id: 42 },
+  }
+);
 
 console.log("result: ", result.messages.at(-1)?.content);
+console.log("followup: ", followup.messages.at(-1)?.content);
